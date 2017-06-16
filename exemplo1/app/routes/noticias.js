83b2d527733219d936ad3e41e4069e3ac9f8eb5a
CDP-NOTE\crist <cristianodp@live.com>
2017-06-16 17:49:08 -0300

    commit teste

--- exemplo1/app/routes/noticias.js
+++ exemplo1/app/routes/noticias.js
@@ -11,7 +11,10 @@ module.exports =function(app){
       host: 'localhost', // Server hosting the postgres database
       port: 5432, //env var: PGPORT
       max: 10, // max number of clients in the pool
-      idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+      idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
+
+
+
     };
 
     const connection = new pg.Pool(config);
