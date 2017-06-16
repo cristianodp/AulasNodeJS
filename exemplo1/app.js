691c69c9a9a006e50aadb684d9170a71bd16691b
CDP-NOTE\crist <cristianodp@live.com>
2017-06-15 21:55:17 -0300

    configuracao views

--- exemplo1/app.js
+++ exemplo1/app.js
@@ -1,12 +1,10 @@
 var app = require("./config/server");
 
+var rotasNoticias = require('./app/routes/noticias')(app);
 
+var rotasHome = require('./app/routes/home')(app);
 
-
-app.get('/tecnologia',function(req,res){
-  res.render("sessao/tecnologia");
-});
-
+var rotasFormAddNoticias = require('./app/routes/form_add_noticia')(app);
 
 app.listen(3000, function(){
 
--- exemplo1/config/server.js
+++ exemplo1/config/server.js
@@ -1,5 +1,5 @@
 var express = require("express");
 var app = express();
 app.set('view engine','ejs');
-app.set('view','./app/views');
+app.set('views','./app/views');
 module.exports = app;
