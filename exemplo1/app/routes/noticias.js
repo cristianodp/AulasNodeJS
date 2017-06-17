d56dc4025bd3137609ebdf871fab5e0398ffd181
CDP-NOTE\crist <cristianodp@live.com>
2017-06-16 21:52:03 -0300

    correçao res.send

--- exemplo1/app/routes/noticias.js
+++ exemplo1/app/routes/noticias.js
@@ -5,10 +5,11 @@ module.exports =function(app){
     const connection = dbConnection();
 
     connection.query("select * from noticias", function(error,result){
-        console.log(result.rowCount + ' rows were received');
+        res.send(result);
+        /*console.log(result.rowCount + ' rows were received');
         var json = JSON.stringify(result.rows, null, "    ");
         console.log("json retorno "+json);
-        res.send(json);
+        res.send(json);*/
     });
 
     //res.render("noticias/noticias");
