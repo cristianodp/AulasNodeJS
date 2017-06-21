9659ba91987f8c02a569d3afffcaec297b4405f0
CDP-NOTE\crist <cristianodp@live.com>
2017-06-21 19:57:02 -0300

    commit chat

--- multiroomChat/app.js
+++ multiroomChat/app.js
@@ -2,6 +2,34 @@
 var app = require('./config/server');
 
 /*parametrizar a porta de escuta*/
-app.listen(80, function(){
+var server = app.listen(80, function(){
   console.log('Servidor online na porta 80');
+});
+
+var io = require('socket.io').listen(server);
+app.set('io',io);
+
+/*Criar coneção para websocket*/
+io.on('connection',function(socket){
+  console.log('Usuário conectou');
+  
+    socket.on('disconnect', function(){
+    console.log('Usuário desconectouuu');
+  });
+
+  socket.on('msgParaServidor',function(data){
+    /*dialogo*/
+    socket.emit('msgParaCliente',{apelido:data.apelido
+                                ,mensagem:data.mensagem});
+
+    socket.broadcast.emit('msgParaCliente',{apelido:data.apelido
+                                ,mensagem:data.mensagem});
+
+    /*participantes*/
+    socket.emit('participantesParaCliente',{apelido:data.apelido});
+
+    socket.broadcast.emit('participantesParaCliente',{apelido:data.apelido});
+
+  });
+
 });
